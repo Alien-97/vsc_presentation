224d61b8f4ca76c3b9dd9fc1f2dedd60781388f8
Alien-97 <alu0101035406@ull.edu.es>
2020-02-10 12:21:51 +0000

    added refactoring examples

---
+++ src/python/1.py
@@ -0,0 +1,20 @@
+
+name = 'Bob'
+
+age = 20
+print("My name is {0}. I am {1} years old. ".format(name,age))
+
+for i in name:
+    print(i)
+
+
+def function_1():
+    pass
+
+def function_add():
+    newvariable460 = (age + 10)
+    return(newvariable460)
+
+function_1()
+function_add()
+
